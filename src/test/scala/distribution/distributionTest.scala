c684af6ddf3385a00ef75680f03e2e07cb9154ea
Eric Janofsky <ebjanofsky@gmail.com>
2016-01-30 20:06:59 -0600

    New test for wishart distns

--- src/test/scala/distribution/distributionTest.scala
+++ src/test/scala/distribution/distributionTest.scala
@@ -1,7 +1,7 @@
 package org.apache.spark.mllib.stat.distribution
 
 import org.scalatest._
-import org.apache.spark.mllib.linalg.{ Vectors, Vector, Matrices, Matrix }
+import org.apache.spark.mllib.linalg.{ Vectors, Vector, Matrices, Matrix, DenseMatrix }
 
 class distributionTest extends FunSuite {
   val alpha = Vectors.dense(1.0, 1.0, 1.0)
@@ -10,5 +10,12 @@ class distributionTest extends FunSuite {
 
   test("Testing density evaluation for dirichlet r.v.") {
     assert(math.abs(dirichlet.pdf(x) - 2.0) <= 1E-3)
+  }
+
+  val nu = 5.0
+  val V = new DenseMatrix(2, 2, Array(1, .5, .5, 5))
+  val wishart = new Wishart(nu, V)
+  test("Testing density evaluation for Wishart r.v.") {
+    assert(wishart.pdf(new DenseMatrix(2, 2, Array(1,0,0,1))).isInstanceOf[Double])
   }
 }
